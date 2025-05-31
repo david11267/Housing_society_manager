9d1b8e4f1c7ee60fa6602b3dff2da484636a3d14
David Aslan <davidaslan112671@live.se>
2025-05-31 12:28:53 +0200

    clerk userbutton theme synced to themeProvider

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,19 @@
-import { UserButton } from '@clerk/clerk-react';
-import { Button } from './ui/button';
-import { ModeToggle } from './mode-toggle';
+import { UserButton } from "@clerk/clerk-react";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./mode-toggle";
+import { useTheme } from "./theme-provider";
+import { dark } from "@clerk/themes";
 
 export default function NavBar() {
+  const { theme } = useTheme();
+
+  const getClerkTheme = () => {
+    if (theme === "system") {
+      return window.matchMedia("(prefers-color-scheme: dark)").matches ? dark : undefined;
+    }
+    return theme === "dark" ? dark : undefined;
+  };
+
   return (
     <div className="flex justify-between">
       <div className="bg-background border-2 rounded-md p-2 space-x-2 inline-flex  ">
@@ -11,9 +22,9 @@ export default function NavBar() {
         <Button variant="outline">Button</Button>
       </div>
 
-      <div className="space-x-4">
+      <div className="space-x-4 dark:">
         <ModeToggle />
-        <UserButton showName />
+        <UserButton appearance={{ baseTheme: getClerkTheme() }} showName />
       </div>
     </div>
   );
