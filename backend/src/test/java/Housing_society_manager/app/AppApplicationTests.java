81b76b72e0b3994c6d24befd6607a5e8bad88cd0
David Aslan <davidaslan112671@live.se>
2025-06-03 22:40:05 +0200

    monorepo build

---
+++ backend/src/test/java/Housing_society_manager/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package Housing_society_manager.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
