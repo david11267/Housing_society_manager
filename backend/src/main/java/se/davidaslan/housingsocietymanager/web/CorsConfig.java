4f31188ea9ca6bb1c877a0e83daf9ed38cfc78d1
David Aslan <davidaslan112671@live.se>
2025-06-04 20:55:53 +0200

    backend is respondig to frontend

--- backend/src/main/java/se/davidaslan/housingsocietymanager/web/CorsConfig.java
+++
@@ -1,4 +0,0 @@
-package se.davidaslan.housingsocietymanager.web;
-
-public class CorsConfig {
-}
--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -1,28 +1,27 @@
-import { fakeData, type HsData } from './types';
+import { type HousingSociety } from "./types";
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = "http://localhost:8080/api";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const postHS = async (data: any, token: string) => {
   const res = await fetch(`${BASE_URL}`, {
-    method: 'POST',
+    method: "POST",
     headers: {
       Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   });
   return res.json();
 };
 
-export const getHS = async (token: string): Promise<HsData[]> => {
-  return fakeData;
-  /*  const res = await fetch(`${BASE_URL}`, {
-    method: 'Get',
+export const getHS = async (token: string): Promise<HousingSociety[]> => {
+  const res = await fetch(`${BASE_URL}`, {
+    method: "Get",
     headers: {
       Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
-  return fakeData.json(); */
+  return res.json();
 };
