419f66b916c345215ab9b9e4d897e5ce07d5d7b9
david <david.aslan1999@gmail.com>
2025-06-05 10:29:25 +0200

    request matcher test

--- backend/src/main/java/se/davidaslan/housingsocietymanager/SecurityConfig.java
+++ backend/src/main/java/se/davidaslan/housingsocietymanager/SecurityConfig.java
@@ -2,6 +2,7 @@ package se.davidaslan.housingsocietymanager;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,7 +17,11 @@ public class SecurityConfig {
     SecurityFilterChain defaultChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api").authenticated()
+                        .requestMatchers(HttpMethod.POST,"/api").authenticated()
+                        .requestMatchers(HttpMethod.PUT,"/api").authenticated()
+                        .requestMatchers(HttpMethod.DELETE,"/api").authenticated()
+                        .requestMatchers("/api").authenticated()
                 )
                 .cors(withDefaults())
                 .oauth2ResourceServer(oauth2  -> oauth2.jwt(withDefaults()))
