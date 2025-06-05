af7a34d3002d531ff61728b2dbe50a9422a7957e
David Aslan <davidaslan112671@live.se>
2025-06-05 02:05:21 +0200

    changed frontend to get root_url(backend) from env variable

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -1,7 +1,6 @@
 import { type HousingSociety } from "./types";
 
-const BASE_URL = "http://localhost:8080/api";
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const postHS = async (data: HousingSociety, token: string) => {
   const res = await fetch(`${BASE_URL}`, {
